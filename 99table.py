4261ee43c250178c1ca2e948ef099120cc91e131
zerlee <1183391310@qq.com>
2018-12-08 23:43:54 +0800

    99乘法表

--- 99table.py
+++ 99table.py
@@ -2,5 +2,6 @@
 
 for i in range(1,10): #依次将1-9赋值给i
     for  j in range(1,i+1): #当i为1时，j为1,当i为2时，j为1,2,当i为3时，j为1,2,3...
-        print('%d*%d=%d'%(j,i,j*i),end=' ') #每输出一个等式后面加一个空格，将两个等式隔开
+        string = '%d*%d=%d'%(j,i,j*i)        
+        print('%-7s'%string,end='') #-7s将等式长度固定为7个字符，循环内的等式不换行
     print('') #换行
