1c268a022eb227012ca6efcc618bf25a7dc0a191
zerlee <1183391310@qq.com>
2018-12-10 19:50:37 +0800

    比赛名单问题

--- bisaimingdan.py
+++ bisaimingdan.py
@@ -4,10 +4,25 @@
 #已抽签决定比赛名单。有人向队员打听比赛的名单。
 #a说他不和x比，c说他不和x,z比，请编程序找出三队赛手的名单。
 
-for a in ['x', 'y', 'z']:                               #a在xyz中挑一个打
-    for b in ['x', 'y', 'z']:                           #b在xyz中挑一个打
-        for c in ['x', 'y', 'z']:                       #c在xyz中挑一个打
-            if a!=b and b!=c and c!=a:                  #a,b,c不能同时挑选同一个人
-                if a!='x' and c!='x' and c!='z':        #依据题意不能ax，cx，cz
-                    print('a'+a, 'b'+b, 'c'+c)
+#for a in ['x', 'y', 'z']:                               #a在xyz中挑一个打
+#    for b in ['x', 'y', 'z']:                           #b在xyz中挑一个打
+#        for c in ['x', 'y', 'z']:                       #c在xyz中挑一个打
+#            if a!=b and b!=c and c!=a:                  #a,b,c不能同时挑选同一个人
+#                if a!='x' and c!='x' and c!='z':        #依据题意不能ax，cx，cz
+#                    print('a'+a, 'b'+b, 'c'+c)
+#
 
+import itertools
+
+jia = ['a','b','c']
+yi = ['x','y','z']
+arr = list(itertools.permutations(yi,3))
+arr = [[jia[i]+a[i] for i in range(3)] for a in arr]
+
+for i in arr:
+    if 'ax' in i:
+        pass
+    elif 'cx' in i or 'cz' in i:
+        pass
+    else:
+        print(i)
