9b2a0128bc4ee49ee56f14a46ac2e0ebcdc15766
zerlee <1183391310@qq.com>
2018-12-11 17:20:48 +0800

    集合set操作

---
+++ set.py
@@ -0,0 +1,13 @@
+#!/sur/bin/python
+
+x = set('runoob')
+y = set('google')
+print(x, y)
+
+print(x & y)
+print(x | y)
+print(x -y)
+
+#print(x.intersection(y))
+#print(x.union(y))
+#print(x.difference(y))
