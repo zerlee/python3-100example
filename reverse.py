b23d9075351c212d48090599df06e216b0dd3ac7
zerlee <1183391310@qq.com>
2018-12-16 11:56:16 +0800

    字符翻转

--- reverse.py
+++ reverse.py
@@ -3,3 +3,14 @@ num = 12345
 s = str(num)
 print(len(s))
 print(s[::-1])
+
+a = [1,2,3,4,5]
+print(a[::-1])
+
+a = [1,2,3,4,5]
+a.reverse()
+print(a)
+
+a = [1,2,3,4,5]
+a.sort(reverse=True)
+print(a)
